b8733c7d35554c1b51182347ec6566b42bbc4dc2
Amer Khalid <amer@amerkhalid.com>
2016-11-20 13:47:51 -0600

    All done

--- app/src/main/java/com/example/android/musicstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicstructure/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicstructure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void playPodcast(View v) {
+        Intent player = new Intent(MainActivity.this, Player.class);
+        startActivity(player);
+    }
+
+    public void discover(View v) {
+        Intent i = new Intent(MainActivity.this, Discover.class);
+        startActivity(i);
+    }
+
+    public void settings(View v) {
+        Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+        startActivity(i);
+    }
+
+    public void subscriptions(View v) {
+        Intent i = new Intent(MainActivity.this, Subscriptions.class);
+        startActivity(i);
     }
 }
