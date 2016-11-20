ca5118026141af28e6b314f6873256d067de2147
Amer Khalid <amer@amerkhalid.com>
2016-11-20 16:05:04 -0600

    Using Listner.

--- app/src/main/java/com/example/android/musicstructure/MainActivity.java
+++ app/src/main/java/com/example/android/musicstructure/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,25 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void playPodcast(View v) {
-        Intent player = new Intent(MainActivity.this, Player.class);
-        startActivity(player);
-    }
+        //Based on example code from Udacity
+        TextView subscriptions = (TextView) findViewById(R.id.activity_subscriptions);
+        subscriptions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Subscriptions.class);
+                startActivity(i);
+            }
+        });
 
-    public void discover(View v) {
-        Intent i = new Intent(MainActivity.this, Discover.class);
-        startActivity(i);
-    }
+        TextView discover = (TextView) findViewById(R.id.activity_discover);
+        discover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Discover.class);
+                startActivity(i);
+            }
+        });
 
-    public void settings(View v) {
-        Intent i = new Intent(MainActivity.this, SettingsActivity.class);
-        startActivity(i);
-    }
+        TextView settings = (TextView) findViewById(R.id.activity_settings);
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
 
-    public void subscriptions(View v) {
-        Intent i = new Intent(MainActivity.this, Subscriptions.class);
-        startActivity(i);
+        // No need to set onClick listener on individual text views.
+        LinearLayout player = (LinearLayout) findViewById(R.id.activity_player);
+        player.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, Player.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
